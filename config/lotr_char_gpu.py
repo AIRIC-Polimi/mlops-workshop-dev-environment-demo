f1d34c0c4e109434c3a214204305d843c2e7623f
Tommaso Bianchi <tommaso.bianchi@polimi.it>
2024-01-14 22:41:31 +0000

    Remove useless TODOs in configs

--- config/lotr_char_gpu.py
+++ config/lotr_char_gpu.py
@@ -10,7 +10,6 @@ log_interval = 50 # don't print too too often
 # we expect to overfit on this small dataset, so only save when val improves
 always_save_checkpoint = False
 
-# TODO: switch to ClearML
 wandb_log = False # override via command line if you like
 wandb_project = 'lotr-char'
 wandb_run_name = 'mini-gpt'
