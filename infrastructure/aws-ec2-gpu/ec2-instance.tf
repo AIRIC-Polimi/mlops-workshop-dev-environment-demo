8192ff642f0130c9af2a93ecf420e802f9cedf8d
Tommaso Bianchi <tommaso.bianchi@polimi.it>
2024-12-01 15:37:06 +0000

    Fix editing in place of ssh config file when it's symlinked

--- infrastructure/aws-ec2-gpu/ec2-instance.tf
+++ infrastructure/aws-ec2-gpu/ec2-instance.tf
@@ -177,8 +177,8 @@ resource "null_resource" "append_ssh_config" {
 
       grep "Host $host" ~/.ssh/config 2>&1 > /dev/null
       if [ $? -eq 0 ]; then
-          sed -i "/Host $host/,/HostName/ s/HostName .*/HostName ${aws_instance.ec2_instance.public_dns}/" ~/.ssh/config
-          sed -i "/Host $host/,/IdentityFile/ s~IdentityFile .*~IdentityFile $identity_file_abspath/ssh_private_key.pem~" ~/.ssh/config
+          sed -i --follow-symlinks "/Host $host/,/HostName/ s/HostName .*/HostName ${aws_instance.ec2_instance.public_dns}/" ~/.ssh/config
+          sed -i --follow-symlinks "/Host $host/,/IdentityFile/ s~IdentityFile .*~IdentityFile $identity_file_abspath/ssh_private_key.pem~" ~/.ssh/config
       else
           ssh_config=$(cat <<END
 
