d3d786a68fb4f35bd2eaed82bf8f2daeb0b43e42
Tommaso Bianchi <tommaso.bianchi@polimi.it>
2024-12-01 17:56:18 +0000

    Run formatter on all python files and fix linting issues

--- config/lotr_char_gpu.py
+++ config/lotr_char_gpu.py
@@ -1,26 +1,26 @@
 # train a miniature character-level lotr model
 # good for debugging and playing on macbooks and such
 
-dataset_dir = 'data/datasets/lotr'
-out_dir = 'out/lotr-char-gpu'
-eval_interval = 250 # keep frequent because we'll overfit
+dataset_dir = "data/datasets/lotr"
+out_dir = "out/lotr-char-gpu"
+eval_interval = 250  # keep frequent because we'll overfit
 eval_iters = 20
-log_interval = 50 # don't print too too often
+log_interval = 50  # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
 always_save_checkpoint = False
 
-wandb_log = False # override via command line if you like
-wandb_project = 'lotr-char'
-wandb_run_name = 'mini-gpt'
+wandb_log = False  # override via command line if you like
+wandb_project = "lotr-char"
+wandb_run_name = "mini-gpt"
 mlflow_log = True
-mlflow_experiment_name = 'lotr-char'
-mlflow_run_name = 'mini-gpt'
+mlflow_experiment_name = "lotr-char"
+mlflow_run_name = "mini-gpt"
 
-dataset = 'lotr_char'
+dataset = "lotr_char"
 gradient_accumulation_steps = 1
 batch_size = 64
-block_size = 256 # context of up to 64 previous characters
+block_size = 256  # context of up to 64 previous characters
 
 # baby GPT model :)
 n_layer = 8
@@ -28,13 +28,13 @@ n_head = 8
 n_embd = 512
 dropout = 0.2
 
-learning_rate = 1e-3 # with baby networks can afford to go a bit higher
+learning_rate = 1e-3  # with baby networks can afford to go a bit higher
 max_iters = 10000
-lr_decay_iters = 10000 # make equal to max_iters usually
-min_lr = 1e-4 # learning_rate / 10 usually
-beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+lr_decay_iters = 10000  # make equal to max_iters usually
+min_lr = 1e-4  # learning_rate / 10 usually
+beta2 = 0.99  # make a bit bigger because number of tokens per iter is small
 
-warmup_iters = 100 # not super necessary potentially
+warmup_iters = 100  # not super necessary potentially
 
-device = 'cuda'
+device = "cuda"
 compile = True
